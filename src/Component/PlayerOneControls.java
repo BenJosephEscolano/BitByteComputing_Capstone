ac2f05f7118545c343ad9e566c5c9567dc907d34
Ben <escolanobenjoseph@gmail.com>
2024-11-24 20:05:06 +0800

    finishing the prototype

--- src/Component/PlayerOneControls.java
+++ src/Component/PlayerOneControls.java
@@ -13,8 +13,8 @@ public class PlayerOneControls extends Controls {
     boolean hasJumped;
     protected Vector lastDirection = new Vector(0, 0);
     protected float targetVelocityX = 0;
-    protected final float acceleration = 1000.0f;
-    protected final float maxSpeed = 500.0f;
+    protected final float acceleration = 3500f;
+    protected final float maxSpeed = 400.0f;
     protected GameObject player;
 
     public PlayerOneControls() {
@@ -30,9 +30,9 @@ public class PlayerOneControls extends Controls {
 
         //adjust velocity gradually towards the target
         if (velocity.getX() < targetVelocityX) {
-            velocity.setX(Math.min(velocity.getX() + acceleration * (float) dt, targetVelocityX));
+            velocity.setX(Math.min(velocity.getX() + acceleration * (float) dt , targetVelocityX));
         } else if (velocity.getX() > targetVelocityX) {
-            velocity.setX(Math.max(velocity.getX() - acceleration * (float) dt, targetVelocityX));
+            velocity.setX(Math.max(velocity.getX() - acceleration * (float) dt , targetVelocityX));
         }
 
         if (keyLisentner.isKeyPressed(KeyEvent.VK_W)) {
@@ -59,22 +59,29 @@ public class PlayerOneControls extends Controls {
     public void jump() {
         if (!hasJumped) {
             Vector velocity = getGameObject().getComponent(RigidBody.class).velocity;
-            velocity.setY(-1000);
+            velocity.setY(-1050);
             hasJumped = true;
         }
     }
 
     public void stop() {
+        Vector velocity = getGameObject().getComponent(RigidBody.class).velocity;
+        //velocity.setX(0);
+
         targetVelocityX = 0; // Slowly bring velocity to 0
     }
 
     @Override
     public void moveLeft() {
+        /*Vector velocity = getGameObject().getComponent(RigidBody.class).velocity;
+        velocity.setX(-500);*/
         targetVelocityX = -maxSpeed;
     }
 
     @Override
     public void moveRight() {
+        /*Vector velocity = getGameObject().getComponent(RigidBody.class).velocity;
+        velocity.setX(500);*/
         targetVelocityX = maxSpeed;
     }
 
